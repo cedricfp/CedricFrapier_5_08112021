46b6d0f16db78f3dc35edcb2d107c82e7f347882
Frapier <cedricfrapier@gmail.com>
2021-12-08 15:51:11 +0100

    changement organisation de script

--- front/js/script.js
+++ front/js/script.js
@@ -1,33 +1,32 @@
 //Requete de l'API avec statut compris entre 200 et 299 au format json
 
 fetch("http://localhost:3000/api/products")
-    .then(function(res){
-        if (res.ok){
-            
-            return res.json();
-            
-        }
-    })
-
-// Transformation du langage json en langage javascript pour recuperation des produits
-
-.then(function(kanapList){
-    console.log(kanapList);
-    //boucle for pour affichage dynamique de tout les canapés
-    for(let kanapDisplay of kanapList) {
-        document.getElementById("items").innerHTML +=
-        `
-        <a href=./product.html?id=${kanapDisplay._id}>
-            <article>
-            <img src=${kanapDisplay.imageUrl} alt="${kanapDisplay.altTxt}>
-            <h3 class="productName">${kanapDisplay.name}</h3>
-            <p class="productDescription">${kanapDisplay.description}.</p>
-            </article>
-        </a>
-        `;
-    }
+.then((res) => res.json())
+.then((items) => {
+    display(items);
 })
+
+// Affichage d'une alerte si les produits ne s'affiche pas 
 .catch(function(err){
-    alert("chargement impossible");
+    alert ("chargement impossible");
     console.log(err);
 });
+// Création de la boucle pour affichage dynamique
+function display(data){
+    for (let product of data){
+        document.getElementById("items").innerHTML += render(product)
+    }
+}
+
+//Récupération de chaque produit de facon dynamique
+function render(product){
+    return `
+    <a href=./product.html?id=${product._id}>
+        <article>
+        <img src=${product.imageUrl} alt="${product.altTxt}>
+        <h3 class="productName">${product.name}</h3>
+        <p class="productDescription">${product.description}.</p>
+        </article>
+    </a>
+    `;
+}
