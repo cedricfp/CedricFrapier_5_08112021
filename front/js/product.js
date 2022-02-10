893eb6c258aea13e6626c4346ccda8a962bbe2ae
Frapier <cedricfrapier@gmail.com>
2022-02-10 16:52:24 +0100

    reprise projet avec modif product

--- front/js/product.js
+++ front/js/product.js
@@ -6,20 +6,35 @@ fetch("http://localhost:3000/api/products/" + id)
 .then((product) => 
 {
     display(product);
-    listenForCartAddition(product);
+    listenForCartAddition();
 })
 
-//Affichage du produit sur la page en fonction du code html
+/**
+ * Affichage du produit sur la page en fonction du code html
+ *
+ * @param   {Object}  product  les caractéristiques d'un produit
+ * @param   {String}  product.imageUrl  le lien vers l'image
+ * @param   {String}  product.name      le nom du produit
+ * @param   {Number}  product.price     le prix
+ * @param   {String}  product.description   sa déscription
+ * @param   {Array}   product.colors    les couleurs dispo
+ * @param   {String}  product.altTxt    le texte alternatif
+ * @return  {void}             manipule le DOM
+ */
 function display(product){
     document.querySelector(".item__img").innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}"></img>`
     document.getElementById("title").innerHTML = product.name;
-    document.getElementById("price").innerHTML = product.price;
+    document.getElementById("price").innerHTML = product.price.toString();
     document.getElementById("description").innerHTML = product.description;
+    
     //boucle for pour allé chercher dans l'API toutes les couleurs disponible
     for (let color of product.colors) {
             document.getElementById("colors").innerHTML += `<option value=${color}>${color}</option>`
     }  
 }  
+
+
+
 //Récuperation de l'id de chaque produit individuellement
 function getDataFromUrl(key){
     let params = new URLSearchParams(document.location.search);
@@ -34,8 +49,13 @@ function listenForCartAddition(){
     document.getElementById("addToCart").addEventListener("click", function (event) {
         event.preventDefault();
         // création des variables de qantité et de couleur
-        let qty = document.getElementById("quantity").value;
-        let color = document.getElementById("colors").value;
+        
+        /** @type {HTMLInputElement} */
+        const DOMqty = document.querySelector("#quantity");
+        let qty = parseInt(DOMqty.value);
+        /** @type {HTMLInputElement} */
+        const DOMcolor = document.querySelector("#colors")
+        let color = DOMcolor.value;
         //création des messages d'alerte pour l'utilisateur
         if (qty < 1){
             alert("veuillez séléctionner une quantité");
@@ -47,7 +67,7 @@ function listenForCartAddition(){
         }
 
         //si les produits existent dans le local storage les prendre
-        let existInLocalStorage = !!localStorage.getItem("products");
+        let existInLocalStorage = localStorage.getItem("products");
 
         //mise au format json
         if (existInLocalStorage){
@@ -58,9 +78,9 @@ function listenForCartAddition(){
             })
 
             //si le produits sont dans le storage les additionner
-            if (!!product)
+            if (product)
             {
-                product.qty = parseInt(product.qty) + parseInt(qty)
+                product.qty = parseInt(product.qty) + qty
                 localStorage.setItem("products", JSON.stringify(items));
             }
 
